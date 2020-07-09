11e523beb01feb33f60ba0c73a3d4cf42a1914d6
Konrad Sperfeld <konrad.sperfeld@uni-rostock.de>
2020-07-09 12:11:46 +0200

    add sentence splitter

---
+++ sentence_splitter.py
@@ -0,0 +1,14 @@
+from somajo import SoMaJo
+
+# :: Tokenize German text ::
+def SentenceSplit(text):
+
+    tokenizer = SoMaJo("de_CMC")
+    tokens = tokenizer.tokenize_text(text)
+    return tokens
+
+if __name__ == "__main__":
+    string=["Ich teste hiermit, ob Sätze ordentlich getrennt werden können. Ich führe diesen Test am 09. Juli durch."]
+    sentences=SentenceSplit(string)
+    for sentence in sentences:
+        print(" ".join([token.text for token in sentence]))
