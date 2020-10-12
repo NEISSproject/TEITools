8028624005edecc1f07a0a75e5827bf9fc6034b3
Konrad Sperfeld <konrad.sperfeld@uni-rostock.de>
2020-10-12 09:24:09 +0200

    added sturm parser

--- sentence_splitter.py
+++ sentence_splitter.py
@@ -11,4 +11,5 @@ if __name__ == "__main__":
     string=["Ich teste hiermit, ob Sätze ordentlich getrennt werden können: Ich führe diesen Test am 09. Juli durch. Erhält man mit diesem Tokenizer eine gute Qualität? Wir werden sehen."]
     sentences=SentenceSplit(string)
     for sentence in sentences:
-        print(" ".join([token.text for token in sentence]))
+        #print(" ".join([token.text for token in sentence]))
+        print(sentence)
