6430623f8a696a7c3778a664af7db8bc68834ef3
Konrad Sperfeld <konrad.sperfeld@uni-rostock.de>
2020-10-12 11:59:15 +0200

    added sturm statistics counter

--- sturm_tei_file_tools.py
+++ sturm_tei_file_tools.py
@@ -80,9 +80,23 @@ def build_ner_data_per_file(directory,outdirectory,fname=None):
             with open(join(outdirectory,filename+'.json'),'w+') as g:
                 json.dump(raw_ner_data,g)
 
+def count_tags_in_json(filelist):
+    tag_collect={}
+    for filename in filelist:
+        with open(filename) as f:
+            training_data=json.load(f)
+        #print(len(training_data))
+        for i in range(len(training_data)):
+            for j in range(len(training_data[i])):
+                if training_data[i][j][1] in tag_collect.keys():
+                    tag_collect[training_data[i][j][1]]+=1
+                else:
+                    tag_collect[training_data[i][j][1]]=1
+    print({k: v for k, v in sorted(tag_collect.items(), key=lambda item: item[1])})
+
 if __name__ == '__main__':
     #build_ner_statistics('../data_sturm/briefe')
-    #build_ner_training_data('../data_040520/briefe','../data_040520/train_data.json')
-    #count_tags_in_json('../data_040520/train_data.json')
+    #build_ner_training_data('../data_sturm/briefe','../data_sturm/train_data.json')
+    count_tags_in_json(['../data_sturm/train_data.json'])
     #split_train_data_in_val_and_train_set('../data_040520/train_data.json','../data_040520/data_uja_ner_train2.json','../data_040520/data_uja_ner_val2.json',0.2)
-    build_ner_data_per_file('../data_sturm/briefe','../data_sturm/data_to_train')
+    #build_ner_data_per_file('../data_sturm/briefe','../data_sturm/data_to_train')
